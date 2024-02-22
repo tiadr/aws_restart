5d8264d5633ff40cd8e8c64d3f5df59d6b7c311a
tiadr <160912692+tiadr@users.noreply.github.com>
2024-02-22 19:27:04 +0200

    Add files via upload

---
+++ categorize-values.py
@@ -0,0 +1,3 @@
+myMixedTypeList = [45,27897897, 1.02, True, "My cat is Chibi.", "45"]
+for item in myMixedTypeList:
+    print("{} is of the data type {}".format(item,type(item)))
---
+++ string-data-type.py
@@ -0,0 +1,13 @@
+myString = "This is my string,"
+print(myString)
+print(type(myString))
+print(myString + " is of the data type " + str(type(myString)))
+firstString = "water"
+secondString = "fall"
+thirdSTring = firstString + secondString
+print(thirdSTring)
+name = input("What is your name?")
+print(name)
+color = input("favourite color?")
+animal = input("favourite animal?")
+print("{}, you like a {} {}".format(name,color, animal))
